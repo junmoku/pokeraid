e0c77d1d9a1d4bcddeccf916eb9997783cb598e4
limsg <kilsoo1234@naver.com>
2025-07-05 14:42:56 +0900

    create room

--- src/room/room.dto.ts
+++ src/room/room.dto.ts
@@ -1,5 +1,20 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
+export class CreateRoomDto {
+
+  @IsString()
+  @IsNotEmpty()
+  roomName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  roomLevel: string;
+
+  @IsString()
+  @IsNotEmpty()
+  usePoketmonId: string;
+}
+
 export class JoinRoomDto {
   @IsString()
   @IsNotEmpty()
