e57261dea098d4d73a8a6a825945551fa452df36
limsg <kilsoo1234@naver.com>
2025-07-26 19:36:15 +0900

    수정

--- docs/sql/user.sql
+++ docs/sql/user.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user` (
   `id` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`seq`),
   UNIQUE KEY `IDX_78a916df40e02a9deb1c4b75ed` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
